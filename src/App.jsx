a5c27397d6950db30de400774f40511607e41dbe
佐藤洋一 <sarub@Yoichi.local>
2022-10-05 16:45:29 +0900

    axiosインストール

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
-import { BrowserRouter, Link, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
 import { InputForm } from './components/InputForm';
 import { PostList } from './components/PostList';
 import { Home } from "./components/Home";
@@ -9,78 +11,82 @@ import { Create } from './components/Create';
 
 function App() {
 
-  const [postList, setPostList] = useState([]);
+  const [title, setTitle] = useState("");
+  const [data, setData] = useState([]);
+  const [isPost, setIsPost] = useState(false);
 
-  // const [data, setData] = useState({ title: "", body: "" });
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/posts").then((res) => {
+      // console.log(res.data);
+      setData(res.data);
+    });
+  }, [isPost]);
 
-  // const handleChange = (e) => {
-  //   setData({
-  //     ...data,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // };
-  
-  // const [addText, setAddText] = useState("");
-  
-  // const onClickAddText = () => {
-  //   setAddText(data.body);
-  //   setData("");
-  // }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+    axios
+      .post(
+        "http://localhost:8000/api/posts",
+        { title }
+        // { withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res);
+        setIsPost(true);
+        setTitle("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log(isPost);
+
+
+
+  const dataList = data.map((item) => {
+    return (
+      <>
+        <p>{item.title}</p>
+      </>
+    );
+  });
 
   return (
+    <div className="App">
 
-    // <BrowserRouter>
-
-    //     <div className="App">
-
-    //       <Route path='/create' Create={Create}/>
-          
-    //     </div>
-    // </BrowserRouter>
-          <div className="App">
-          
-            <h1>TEST POST APP</h1>
-            <p>Home</p>
-  
-            <InputForm postList={postList} setPostList={setPostList}/>
-            <PostList postList={postList} setPostList={setPostList}/>
-
-          </div>
-        
-        
-        /* <div>
-          <Create /><hr/>
-          <Route exact path='/' component={App}/>
-          <Route path='/Create' component={Create}/>
-        </div> */
-
-
-
-        /* <label>Title:</label>
-        <input type="text" name='title' onChange={handleChange}/>
-
-        <label>Body:</label>
-        <input type="text" name='body' onChange={handleChange}/>
-        <button onClick={onClickAddText}>追加</button>
-
-        <p>ボタンクリック：{addText}</p>
-
-
-
-        <br />
-        title: {data.title}
-        <br />
-        body: {data.body} */
-
-        /* <input value={text} onChange={(event) => setText(event.target.value)} placeholder="Title"/>
-        <input value={body} onChange={(event) => setBody(event.target.value)} placeholder="Body"/>
-        <button onClick={onClickAddText}>追加</button> */
-        
-        
-        /* <p>Title:{text}</p>
-        <p>Body:{body}</p> */
-
+      {/* 画面遷移の記述 */}
+      {/* <Link to="/">home</Link>
+      <Link to="/create">create</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes> */}
+      
+      {/* API連携の記述 */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {dataList}
+    </div>
   );
+  // const [postList, setPostList] = useState([]);
+  //         <div className="App">
+          
+  //           <h1>TEST POST APP</h1>
+  //           <p>Home</p>
+  
+  //           <InputForm postList={postList} setPostList={setPostList}/>
+  //           <PostList postList={postList} setPostList={setPostList}/>
+
+  //         </div>
 }
 
 export default App;
